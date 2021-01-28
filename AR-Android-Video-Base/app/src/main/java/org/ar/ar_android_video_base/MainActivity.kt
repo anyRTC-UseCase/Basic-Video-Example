d5f51b2dfe0e6442a7d8d1612f2afc68dbc46b1a
东慕雨 <1872676224@qq.com>
2021-01-28 16:22:08 +0800

    更新android视频通话

--- AR-Android-Video-Base/app/src/main/java/org/ar/ar_android_video_base/MainActivity.kt
+++ AR-Android-Video-Base/app/src/main/java/org/ar/ar_android_video_base/MainActivity.kt
@@ -3,7 +3,9 @@ package org.ar.ar_android_video_base
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
@@ -19,7 +21,9 @@ class MainActivity : AppCompatActivity(),View.OnClickListener{
         super.onCreate(savedInstanceState)
         viewBinding=ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(viewBinding?.root)
+        viewBinding?.input?.addTextChangedListener(textWatcher)
         viewBinding?.join?.setOnClickListener(this)
+        viewBinding?.join?.isEnabled =false
         if (!AndPermission.hasPermissions(this, Permission.Group.STORAGE,
                         Permission.Group.MICROPHONE)){
             AndPermission.with(this)
@@ -32,16 +36,20 @@ class MainActivity : AppCompatActivity(),View.OnClickListener{
         }
     }
 
+    private var textWatcher: TextWatcher =object : TextWatcher {
+        override fun afterTextChanged(p0: Editable?) {
+            viewBinding?.join?.isEnabled = p0?.toString()?.length!! > 0
+        }
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+    }
+
     override fun onClick(p0: View?) {
         when(p0?.id){
             R.id.join->{
-                if (!TextUtils.isEmpty(viewBinding?.input?.text.toString())){
-                    Intent(this,VideoBaseActivity::class.java).let {
-                        it.putExtra("channelId",viewBinding?.input?.text.toString())
-                        startActivity(it)
-                    }
-                }else{
-                    Toast.makeText(this, "请输入频道ID", Toast.LENGTH_SHORT).show()
+                Intent(this,VideoBaseActivity::class.java).let {
+                    it.putExtra("channelId",viewBinding?.input?.text.toString())
+                    startActivity(it)
                 }
             }
         }
